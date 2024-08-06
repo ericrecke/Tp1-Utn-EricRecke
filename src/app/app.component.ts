9967f5e9122e0b9d70231c6665e6ab8c4e59af54
Eric Recke <ericrecke@gmail.com>
2024-08-05 21:10:31 -0300

    Project Tp 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Tp1-Utn-EricRecke';
+  username = "Usuario";
+}
